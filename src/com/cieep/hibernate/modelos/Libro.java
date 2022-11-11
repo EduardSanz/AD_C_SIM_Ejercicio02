ab0cd059569329f79fa5a83c39fe034de913ec68
eduard <eduard@cieep.com>
2022-11-11 20:59:25 +0100

    Initial commit

---
+++ src/com/cieep/hibernate/modelos/Libro.java
@@ -0,0 +1,66 @@
+package com.cieep.hibernate.modelos;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "libros")
+public class Libro implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String titulo;
+
+    @ManyToOne
+    @JoinColumn(name = "id_autor")
+    private Autor autor;
+
+    @OneToMany(mappedBy = "libro", cascade = CascadeType.ALL, orphanRemoval = true)
+    private ArrayList<Alquiler> alquires;
+
+    public Libro() {
+        alquires = new ArrayList<>();
+    }
+
+    public Libro(int id, String titulo) {
+        this.id = id;
+        this.titulo = titulo;
+        alquires = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public ArrayList<Alquiler> getAlquires() {
+        return alquires;
+    }
+
+    public void setAlquires(ArrayList<Alquiler> alquires) {
+        this.alquires = alquires;
+    }
+}
