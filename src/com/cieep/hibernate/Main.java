c4d446f10db217d0f5c7f4ca79f389ef9f9e89cc
eduard <eduard@cieep.com>
2022-11-11 21:11:13 +0100

    Falta arreglar el error final

--- src/com/cieep/hibernate/Main.java
+++ src/com/cieep/hibernate/Main.java
@@ -1,7 +1,43 @@
 package com.cieep.hibernate;
 
+import com.cieep.hibernate.modelos.Abonado;
+import com.cieep.hibernate.modelos.Alquiler;
+import com.cieep.hibernate.modelos.Autor;
+import com.cieep.hibernate.modelos.Libro;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Abonado abonado = new Abonado();
+        abonado.setNombre("Manolito");
+        Autor autor = new Autor();
+        autor.setNombre("Javier");
+        Libro libro = new Libro();
+        libro.setTitulo("Por qué no dedicarse a la Programación");
+        libro.setAutor(autor);
+        Alquiler alquiler = new Alquiler();
+        alquiler.setFecha(Date.valueOf(LocalDate.now()));
+        abonado.getAlquileres().add(alquiler);
+        libro.getAlquires().add(alquiler);
+        autor.getLibros().add(libro);
+
+        // CREA LA VARIABLE QUE PUEDE CONECTAR
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        // LA VARIBLE DE LA CONEXiÓN
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        session.persist(abonado);
+        session.persist(autor);
+        session.persist(libro);
+        session.persist(alquiler);
+
+        session.getTransaction().commit();
+        session.close();
     }
 }
